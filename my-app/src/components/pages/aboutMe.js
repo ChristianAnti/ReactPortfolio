855362f10e120ec54b2414156cb7ac5537b3b598
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2022-02-23 15:53:02 -0500

    adding basics for rendering page and the about me section with basic info

--- my-app/src/components/pages/aboutMe.js
+++ my-app/src/components/pages/aboutMe.js
@@ -1,2 +1,19 @@
 import React from "react";
 
+export default function aboutMe() {
+    return (
+        <>
+        <section>
+            <h1> Meet Christian Gleason </h1>
+            <img
+            className="me"
+            alt="Monkey at keyboard"
+            src={require('../../assets/MeInRealLife.jpg')}
+            />
+            <p>
+                Hi I'm Christian im a full stack developer. :)
+            </p>
+        </section>
+        </>
+    );
+}
--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,2 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import aboutMe from "./pages/aboutMe";
+import myWork from "./pages/myWork";
+import contactMe from "./pages/contactMe";
 
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState("aboutMe");
+
+
+    const renderPage = () => {
+        if (currentPage === "aboutMe") {
+            return <aboutMe/>;
+        }else if (currentPage === "aboutMe") {
+            return <aboutMe/>;
+        }else if (currentPage === "aboutMe") {
+            return <aboutMe/>;
+        }else if (currentPage === "aboutMe") {
+            return <aboutMe/>;
+        }
+    }
+
+}
